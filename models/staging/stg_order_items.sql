b8aac96360cdfba3a285ebe98d3073d6e0913b45
Michael Zhang <michael.zhang@navex.com>
2025-05-16 13:14:18 -0500

    switch back stg_order_items

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -1,33 +1,12 @@
-{{
-  config(
-    materialized = 'incremental',
-    unique_key = 'order_item_id',
-    on_schema_change = 'sync_all_columns',
-    merge_exclude_columns = ['created_at'],
-    )
-}}
-
-
 with source as (select * from {{ source('ecom', 'raw_items') }}),
 
 renamed as (
     select
         id as order_item_id,
         order_id,
-        sku as product_id,
-        'dbt' as update_by,
-        dateadd(
-            day,
-            uniform(0, datediff(day, '2025-01-01', '2025-05-01'), random()),
-            to_date('2025-01-01')
-        ) as created_at,
-        current_timestamp() as updated_at
-
+        sku as product_id
     from source
 )
 
 select * from
     renamed
-    {% if is_incremental() %}
-        sample (1)
-    {% endif %}
