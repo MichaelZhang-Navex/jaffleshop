f9b628434bea9fb125a8cb1c9436dd884bb6dbca
Michael Zhang <michael.zhang@navex.com>
2025-05-15 19:01:06 -0500

    remove random things

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -1,12 +1,9 @@
 {{
   config(
-    materialized = 'incremental',
-    unique_key = 'order_item_id',
+    materialized = 'table',
     on_schema_change = 'sync_all_columns',
-    merge_exclude_columns = ['created_at'],
     )
 }}
-
 
 with source as (select * from {{ source('ecom', 'raw_items') }}),
 
@@ -14,20 +11,10 @@ renamed as (
     select
         id as order_item_id,
         order_id,
-        sku as product_id,
-        'dbt' as update_by,
-        dateadd(
-            day,
-            uniform(0, datediff(day, '2025-01-01', '2025-05-01'), random()),
-            to_date('2025-01-01')
-        ) as created_at,
-        current_timestamp() as updated_at
-
+        sku as product_id
     from source
 )
 
-select * from
+select *
+from
     renamed
-    {% if is_incremental() %}
-        sample (1)
-    {% endif %}
